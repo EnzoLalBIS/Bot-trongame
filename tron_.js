004e5508a68ddc636f284593058963e27a166a68
Enzo Lallet <enzolalbis@gmail.com>
2025-11-07 14:25:25 +0100

    first version

---
+++ tron_.js
@@ -0,0 +1,40 @@
+
+class Bot {
+  constructor(name, linkedBike) {
+    this.name = name;
+    this.linkedBike = linkedBike;
+  }
+
+  // Put your code here
+  // This should only return an array containing the choosen coordinates
+  // Ex: [2, 1]
+  
+  getMove(arena) {
+    let opts = []
+    let maxi_tiles=1
+    let legalMoves=arena.getLegalMoves(this.linkedBike.x, this.linkedBike.y)
+    for (let index = 0; index < legalMoves.length; index++) {
+      if (legalMoves[index].collision == false){
+        let TilesNum=arena.getAvailableTilesNumber(legalMoves[index].xMove, legalMoves[index].yMove)
+        if (TilesNum > maxi_tiles){
+          opts=[]
+          opts.push([legalMoves[index].xMove, legalMoves[index].yMove])
+          maxi_tiles=TilesNum
+        }
+        else if (TilesNum == maxi_tiles){
+          opts.push([legalMoves[index].xMove, legalMoves[index].yMove])
+        }
+      }
+    }
+    if (opts.length===0) {
+      return [legalMoves[0].xMove, legalMoves[0].yMove]
+    }
+    else if(opts.length===1){
+      return opts[0]
+    }
+    else {
+      let nb_rnd=Math.floor(Math.random() * opts.length);
+      return opts[nb_rnd]
+    }
+  }
+}
